eb2589e7c2c1bb6c30e1043746d12d4727fedd4e
jesshdzz <jesushernandezz.dev@gmail.com>
2026-02-04 23:27:34 -0600

    eliminar componentes no usados

--- src/actions/product-actions.ts
+++ src/actions/product-actions.ts
@@ -3,7 +3,6 @@
 import { prisma } from "@/lib/prisma";
 import { productSchema } from "@/lib/validators/product-schema";
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 // --- OBTENER PRODUCTOS ---
 export async function getProducts() {
